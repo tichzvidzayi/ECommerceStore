725b3c8524870f944aad9571b788061b04db9896
Tich Z <tzvidzie@gmail.com>
2024-03-07 00:47:17 +0200

    update

--- fakeapi_store/src/contexts/ProductContext.js
+++ fakeapi_store/src/contexts/ProductContext.js
@@ -1,28 +1,29 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useCallback, useEffect } from "react";
 
 export const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+ 
+  const fetchProducts = useCallback(async () => {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products`);
+
+      if (!response.ok) {
+        // If the response status is not OK (e.g., 404 Not Found), throw an error
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error.message || "An error occurred");
+    }
+  }, []);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(`https://fakestoreapi.com/products`);
-
-        if (!response.ok) {
-          // If the response status is not OK (e.g., 404 Not Found), throw an error
-          throw new Error("Network response was not ok");
-        }
-        
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.log(error.message || "An error occurred");
-      }
-    };
     fetchProducts();
-  }, []);
+  }, [fetchProducts]);
 
   return (
     <ProductContext.Provider value={{ products }}>
